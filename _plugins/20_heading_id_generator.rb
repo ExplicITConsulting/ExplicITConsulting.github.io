afd7f2c0d47932fe29d731b57f83d4b1463aad10
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-08-10 17:53:40 +0200

    refactor: enhance heading ID assignment logic and improve HTML modification process

--- _plugins/20_heading_id_generator.rb
+++ _plugins/20_heading_id_generator.rb
@@ -1,53 +1,18 @@
-require 'nokogiri'
-require 'uri'
-require 'set' # Added this line
+# _plugins/add_heading_ids_safe.rb
 
-module Jekyll
-  class HeadingIdGenerator
-    Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
-      next unless doc.output.is_a?(String) && doc.output.strip.start_with?('<')
-      next if doc.url == '/search.json' || doc.data['sitemap'] == false
-      next if doc.output.include?('<meta http-equiv="refresh"')
+Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
+  # Only process HTML files
+  next unless doc.output_ext == '.html'
 
-      Jekyll.logger.info "HeadingIdGenerator:", "Processing #{doc.url || doc.path} for missing heading IDs..."
+  id_counter = 1
 
-      doc_fragment = Nokogiri::HTML.fragment(doc.output)
-      all_headings = doc_fragment.css('h1, h2, h3, h4, h5, h6')
-      counter = 1
-      
-      existing_ids = Set.new(all_headings.map { |h| h['id'] }.compact)
+  # This regex matches <h1> ... <h6> tags that do NOT have an id= attribute
+  # and captures the start tag (group 1) and the rest of the tag name & attributes (group 2)
+  regex = /<(h[1-6])\b(?![^>]*\bid=)([^>]*)>/i
 
-      modified_html = doc.output
-
-      all_headings.each do |heading_element|
-        if heading_element['id'].nil? || heading_element['id'].empty?
-          # Generate a new ID and ensure it's unique
-          new_id = "heading-#{counter}"
-          while existing_ids.include?(new_id)
-            counter += 1
-            new_id = "heading-#{counter}"
-          end
-          
-          # Store the original and new HTML
-          original_html = heading_element.to_s
-          heading_element['id'] = new_id
-          new_html = heading_element.to_s
-
-          # Use a more robust substitution to avoid issues
-          modified_html = modified_html.sub(original_html, new_html)
-
-          existing_ids.add(new_id)
-          Jekyll.logger.info "HeadingIdGenerator:", " Assigned new ID: '#{new_id}' to a heading."
-        else
-          Jekyll.logger.info "HeadingIdGenerator:", " Heading already has ID: '#{heading_element['id']}'."
-        end
-        counter += 1
-      end
-
-      # Update the document's output with the new IDs
-      doc.output = modified_html
-      
-      Jekyll.logger.info "HeadingIdGenerator:", "Finished processing #{doc.url || doc.path}."
-    end
+  doc.output = doc.output.gsub(regex) do
+    tag_name = Regexp.last_match(1)
+    rest = Regexp.last_match(2)
+    %Q{<#{tag_name} id="heading-#{id_counter}"#{rest}>}.tap { id_counter += 1 }
   end
 end
